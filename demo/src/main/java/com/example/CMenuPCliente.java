759a30ca4e2e3ce5e11c480682ed0ef19a2fec0a
GianEsteves <115878514+GianEsteves@users.noreply.github.com>
2023-04-30 16:22:53 -0300

    comçando as telas

---
+++ demo/src/main/java/com/example/CMenuPCliente.java
@@ -0,0 +1,26 @@
+package com.example;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class CMenuPCliente {
+
+    @FXML
+    private Button ListarClienteIF;
+
+    @FXML
+    private Button adicionarNoFim;
+
+    @FXML
+    private Button adicionarNoInicio;
+
+    @FXML
+    private Button editarCliente;
+
+    @FXML
+    private Button listarClienteFI;
+
+    @FXML
+    private Button removerCliente;
+
+}
--- demo/src/main/java/com/example/CmenuInicial.java
+++ demo/src/main/java/com/example/CmenuInicial.java
@@ -1,11 +1,39 @@
 package com.example;
 
+import java.io.IOException;
+
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.stage.Stage;
 
 public class CmenuInicial {
 
     @FXML
-    private Button menuVeiculo;
+    private Button menuCategoria;
 
+    @FXML
+    private Button menuCliente;
+
+    @FXML
+    private Button menuLocacao;
+
+    @FXML
+    private Button menuVeiculo;
+    
+    @FXML
+    public void abrirFXML() throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("MenuPCliente.fxml"));
+        Parent root = loader.load();
+        Scene scene = new Scene(root);
+        Stage stage = new Stage();
+        stage.setScene(scene);
+        stage.show();
+    
+        CMenuPCliente controller = loader.getController();
+        // fazer algo com o controlador, se necessário
+    }
 }
+
