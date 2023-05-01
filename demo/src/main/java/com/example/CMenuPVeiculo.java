d3eda12d5d3f3d00bb4bb3b6d930c86f6d2a5ef6
GianEsteves <115878514+GianEsteves@users.noreply.github.com>
2023-05-01 14:58:53 -0300

    menu add veiculo no inicio e no fim

--- demo/src/main/java/com/example/CMenuPVeiculo.java
+++ demo/src/main/java/com/example/CMenuPVeiculo.java
@@ -1,7 +1,13 @@
 package com.example;
 
+import java.io.IOException;
+
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.stage.Stage;
 
 public class CMenuPVeiculo {
 
@@ -22,5 +28,31 @@ public class CMenuPVeiculo {
 
     @FXML
     private Button listarVeiculoDoInicioAoFim;
+
+    @FXML
+    public void abrirFXMLMCadastrarI() throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("MenuVeiculoAdd.fxml"));
+        Parent root = loader.load();
+        Scene scene = new Scene(root);
+        Stage stage = new Stage();
+        stage.setScene(scene);
+        stage.show();
+    
+        //CMenuPCliente controller = loader.getController();
+        // fazer algo com o controlador, se necessário
+    }
+
+    @FXML
+    public void abrirFXMLMCadastrarF() throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("MenuVeiculoAddF.fxml"));
+        Parent root = loader.load();
+        Scene scene = new Scene(root);
+        Stage stage = new Stage();
+        stage.setScene(scene);
+        stage.show();
+    
+        //CMenuPCliente controller = loader.getController();
+        // fazer algo com o controlador, se necessário
+    }
 
 }
---
+++ demo/src/main/java/com/example/CMenuVeiculoAddF.java
@@ -0,0 +1,39 @@
+package com.example;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class CMenuVeiculoAddF {
+
+    @FXML
+    private Button cadastrar;
+
+    @FXML
+    private Button limpar;
+
+    @FXML
+    private TextField salvaAno;
+
+    @FXML
+    private TextField salvaCategoria;
+
+    @FXML
+    private TextField salvaMarca;
+
+    @FXML
+    private TextField salvaModelo;
+
+    @FXML
+    private TextField salvaNLugares;
+
+    @FXML
+    private TextField salvaPlaca;
+
+    @FXML
+    private TextField salvaPotencia;
+
+    @FXML
+    private Button voltar;
+
+}
