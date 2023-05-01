4086ce2974b4031406d2bf1dfca5fc65cb393351
GianEsteves <115878514+GianEsteves@users.noreply.github.com>
2023-05-01 15:38:57 -0300

    add locação

---
+++ demo/src/main/java/com/example/CMenuLocacaoAdd.java
@@ -0,0 +1,21 @@
+package com.example;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class CMenuLocacaoAdd {
+
+    @FXML
+    private Button alocar;
+
+    @FXML
+    private Button foltar;
+
+    @FXML
+    private TextField salvaCPF;
+
+    @FXML
+    private TextField salvaPlaca;
+
+}
--- demo/src/main/java/com/example/CMenuPLocacao.java
+++ demo/src/main/java/com/example/CMenuPLocacao.java
@@ -1,7 +1,13 @@
 package com.example;
 
+import java.io.IOException;
+
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.stage.Stage;
 
 public class CMenuPLocacao {
 
@@ -13,5 +19,18 @@ public class CMenuPLocacao {
 
     @FXML
     private Button listarLocacao;
+
+    @FXML
+    public void abrirFXMLMadd() throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("MenuLocacaoAdd.fxml"));
+        Parent root = loader.load();
+        Scene scene = new Scene(root);
+        Stage stage = new Stage();
+        stage.setScene(scene);
+        stage.show();
+    
+        //CMenuPCliente controller = loader.getController();
+        // fazer algo com o controlador, se necessário
+    }
 
 }
