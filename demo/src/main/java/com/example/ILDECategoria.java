4e056bc2d985514b42a958c1d7faba8fb65d1ec1
GianEsteves <115878514+GianEsteves@users.noreply.github.com>
2023-05-01 20:26:30 -0300

    ataulizacao

--- demo/src/main/java/com/example/ILDECategoria.java
+++ demo/src/main/java/com/example/ILDECategoria.java
@@ -15,5 +15,46 @@ public interface ILDECategoria {
     void editarCategoria(int codCategoria);
 
     void excluirCategoria(int codCategoria);
+    /*
+     * public Categoria excluirCategoria(String nomeCategoria) throws Exception {
+     * Categoria categoriaAtual = primeiraCategoria;
+     * 
+     * // Procura pela categoria a ser excluída
+     * while (categoriaAtual != null &&
+     * !categoriaAtual.getNome().equals(nomeCategoria)) {
+     * categoriaAtual = categoriaAtual.getProxima();
+     * }
+     * 
+     * // Verifica se a categoria existe na lista encadeada
+     * if (categoriaAtual == null) {
+     * throw new Exception("Categoria não encontrada");
+     * }
+     * 
+     * // Verifica se existem veículos atrelados à categoria
+     * Veiculo veiculoAtual = primeiroVeiculo;
+     * while (veiculoAtual != null) {
+     * if (veiculoAtual.getCategoria().equals(categoriaAtual)) {
+     * throw new Exception("Existem veículos atrelados à categoria " +
+     * nomeCategoria);
+     * }
+     * veiculoAtual = veiculoAtual.getProximo();
+     * }
+     * 
+     * // Atualiza os ponteiros da lista encadeada
+     * if (categoriaAtual == primeiraCategoria) {
+     * primeiraCategoria = categoriaAtual.getProxima();
+     * } else {
+     * Categoria categoriaAnterior = primeiraCategoria;
+     * while (categoriaAnterior.getProxima() != categoriaAtual) {
+     * categoriaAnterior = categoriaAnterior.getProxima();
+     * }
+     * categoriaAnterior.setProxima(categoriaAtual.getProxima());
+     * }
+     * 
+     * // Limpa os ponteiros da categoria excluída e retorna ela
+     * categoriaAtual.setProxima(null);
+     * return categoriaAtual;
+     * }
+     */
 
 }
