2da33a68da807f135ceee515a997c88b6ae1eae9
GianEsteves <115878514+GianEsteves@users.noreply.github.com>
2023-05-01 21:51:07 -0300

    atualização

--- demo/src/main/java/com/example/CMenuAddClienteF.java
+++ demo/src/main/java/com/example/CMenuAddClienteF.java
@@ -1,8 +1,16 @@
 package com.example;
 
+import java.io.IOException;
+
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.scene.control.Alert.AlertType;
+import javafx.stage.Stage;
 
 public class CMenuAddClienteF {
 
@@ -26,5 +34,52 @@ public class CMenuAddClienteF {
 
     @FXML
     private Button voltar;
+
+    @FXML
+    void adicionaDliente() throws Exception {
+
+        String cpf = salvaCPF.getText();
+        String cnh = salvaCNH.getText();
+        String telefone = salvaTelefone.getText();
+        String nome = salvaNome.getText();
+     
+        
+        // Cria um novo objeto NohCliente com as informações fornecidas
+        //NohCliente  novoNoh = new NohCliente(nome, 0, 0, 0)
+        NohCliente novoNoh = new NohCliente(nome, cnh, telefone, cpf);
+        LDECliente lc = new LDECliente(null, null);
+        lc.adicionarNoCliente(novoNoh);
+        lc.listarCliente();
+        //limpar();
+        
+        //limparCampos(null);
+        
+        alertInterface("SUCESSO", "Cliente adicionado com sucesso!", Alert.AlertType.INFORMATION);
+    }
+
+    private void alertInterface(String string, String string2, AlertType information) {
+        //System.out.print("foi");
+    }
+
+    @FXML
+    void limpar(){
+        salvaCPF.setText("");
+        salvaCNH.setText("");
+        salvaTelefone.setText("");
+        salvaNome.setText("");
+    }
+
+    @FXML
+    void abrirFXMLIinicial() throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("MenuPrincipal.fxml"));
+        Parent root = loader.load();
+        Scene scene = new Scene(root);
+        Stage stage = new Stage();
+        stage.setScene(scene);
+        stage.show();
+    
+        //CMenuPCliente controller = loader.getController();
+        // fazer algo com o controlador, se necessário
+    }
 
 }
