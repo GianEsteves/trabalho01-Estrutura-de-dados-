ca719f90b36cfe0a21520a1c54ed7ef0c029b6c2
GianEsteves <115878514+GianEsteves@users.noreply.github.com>
2023-05-02 09:16:18 -0300

    aaaaaaaaaaaaaaaaaaa

--- demo/src/main/java/com/example/CMenuVeiculoExcluir.java
+++ demo/src/main/java/com/example/CMenuVeiculoExcluir.java
@@ -1,8 +1,14 @@
 package com.example;
 
+import java.io.IOException;
+
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
 public class CMenuVeiculoExcluir {
 
@@ -10,7 +16,7 @@ public class CMenuVeiculoExcluir {
     private Button buscarPlaca;
 
     @FXML
-    private Button escluir;
+    private Button excluir;
 
     @FXML
     private TextField salvaAbuscadaPlaca;
@@ -18,4 +24,21 @@ public class CMenuVeiculoExcluir {
     @FXML
     private Button voltar;
 
+    @FXML
+    void abrirFXMLIinicial() throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("MenuPrincipal.fxml"));
+        Parent root = loader.load();
+        Scene scene = new Scene(root);
+        Stage stage = new Stage();
+        stage.setScene(scene);
+        stage.show();
+    
+        //CMenuPCliente controller = loader.getController();
+        // fazer algo com o controlador, se necessário
+    }
+
+    @FXML
+    void excluir(){
+        
+    }
 }
